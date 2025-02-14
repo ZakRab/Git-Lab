687da6c9dc9718bbf61267a70f7e02164ade33a4
Kenny Morales <knnymrls@outlook.com>
2025-02-14 15:14:57 -0600

    Revert "fix multiplication function and update tests"
    This reverts commit f40fb29d288586fae5e3354eeda99a287cc70a92.

--- git_lab.py
+++ git_lab.py
@@ -70,12 +70,10 @@ def add_numbers(a, b):
     """Returns the sum of two numbers."""
     return a + b
 
-
 def subtract_numbers(a, b):
     """Returns the difference of two numbers."""
     return a - b
 
-
 def multiply_numbers(a, b):
     """Returns the product of two numbers."""
-    return a * b
+    return a / b
